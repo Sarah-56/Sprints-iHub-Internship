57f5ee9a5436567e3077fc65630c6e0884006d79
Sarah-56 <59021799+Sarah-56@users.noreply.github.com>
2021-09-09 18:09:30 +0200

    Car_project

---
+++ Embedded-L1/Car/CAR_Test/Debug/Application/App.d
@@ -0,0 +1,41 @@
+Application/App.o Application/App.o: ../Application/App.c \
+  ../Application/App.h ../Application/../ECUAL/Button/buttons.h \
+  ../Application/../ECUAL/Button/../../std_types.h \
+  ../Application/../ECUAL/Button/../../common_macros.h \
+  ../Application/../ECUAL/Button/../../registers.h \
+  ../Application/../ECUAL/Button/../../std_types.h \
+  ../Application/../ECUAL/Button/../../MCAL/DIO/DIO.h \
+  ../Application/../ECUAL/Button/../../MCAL/DIO/../../registers.h \
+  ../Application/../ECUAL/Motor/motors.h \
+  ../Application/../ECUAL/Motor/../../MCAL/DIO/DIO.h \
+  ../Application/../ECUAL/Motor/../../MCAL/timer/timer.h \
+  ../Application/../ECUAL/Motor/../../MCAL/timer/../../registers.h \
+  ../Application/../micro_config.h ../Application/../ECUAL/Motor/motors.h
+
+../Application/App.h:
+
+../Application/../ECUAL/Button/buttons.h:
+
+../Application/../ECUAL/Button/../../std_types.h:
+
+../Application/../ECUAL/Button/../../common_macros.h:
+
+../Application/../ECUAL/Button/../../registers.h:
+
+../Application/../ECUAL/Button/../../std_types.h:
+
+../Application/../ECUAL/Button/../../MCAL/DIO/DIO.h:
+
+../Application/../ECUAL/Button/../../MCAL/DIO/../../registers.h:
+
+../Application/../ECUAL/Motor/motors.h:
+
+../Application/../ECUAL/Motor/../../MCAL/DIO/DIO.h:
+
+../Application/../ECUAL/Motor/../../MCAL/timer/timer.h:
+
+../Application/../ECUAL/Motor/../../MCAL/timer/../../registers.h:
+
+../Application/../micro_config.h:
+
+../Application/../ECUAL/Motor/motors.h:
