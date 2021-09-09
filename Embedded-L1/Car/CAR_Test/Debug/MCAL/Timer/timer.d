b30c2f9f0bf3d206e0fe68d77a5799b0fca23d75
Sarah-56 <59021799+Sarah-56@users.noreply.github.com>
2021-09-09 17:56:25 +0200

    Delete Embedded-L1/Car directory

--- Embedded-L1/Car/CAR_Test/Debug/MCAL/Timer/timer.d
+++
@@ -1,9 +0,0 @@
-MCAL/Timer/timer.o MCAL/Timer/timer.o: ../MCAL/Timer/timer.c \
-  ../MCAL/Timer/timer.h ../MCAL/Timer/../../registers.h \
-  ../MCAL/Timer/../../std_types.h
-
-../MCAL/Timer/timer.h:
-
-../MCAL/Timer/../../registers.h:
-
-../MCAL/Timer/../../std_types.h:
